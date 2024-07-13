df38280e597070cdcc8bce3129589ae2f77e990e
AmedMRashed <64852704+AmedMRashed@users.noreply.github.com>
2024-07-13 19:42:58 +0300

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,87 @@
+let footer= document.querySelector(".footer");
+
+if(theme_key == 1 ){
+    footer.innerHTML= "<footer class=\"\">\n" +
+        "    <div class=\"container\">\n" +
+        "        <div class=\"row\">\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box\">\n" +
+        "                    <div class=\"brand\">\n" +
+        "                        <img src=\"img/logo.png\" alt=\"\" />\n" +
+        "                        <span class='dokan'>  <spsn> متجر </span> <p> </p> <span>  فخر </span>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box-two\">\n" +
+        "                    <div class=\"icons\">\n" +
+        "                        <span><i class=\"fa fa-facebook\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-youtube\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-instagram\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-twitter\"></i></span>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box\">\n" +
+        "                    <div class=\"contact-us\">\n" +
+        "                        <div class=\"content\">\n" +
+        "                            <p>تواصل على الخط الساخن</p>\n" +
+        "                            <p>0567283751 +</p>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"icon\">\n" +
+        "                            <i class=\"fa fa-headphones-simple\"></i>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "</footer>\n" +
+        "<div class=\"copy-right\">\n" +
+        "    <p>حقوق الطبع والنشر © لعام 2022 محفوظة لمتجر <span>فخر</span></p>\n" +
+        "</div>" ;
+}else {
+    footer.innerHTML= "<footer class=\"\">\n" +
+        "    <div class=\"container\">\n" +
+        "        <div class=\"row\">\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box\">\n" +
+        "                    <div class=\"brand\">\n" +
+        "                        <img src=\"img/logo2.png\" alt=\"\" />\n" +
+        "                        <span class='dokan'> متجر فخر </span>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box-two\">\n" +
+        "                    <div class=\"icons\">\n" +
+        "                        <span> <i class=\"fa fa-facebook\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-youtube\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-instagram\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-twitter\"></i></span>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box\">\n" +
+        "                    <div class=\"contact-us\">\n" +
+        "                        <div class=\"content\">\n" +
+        "                            <p>تواصل على الخط الساخن</p>\n" +
+        "                            <p>+</p>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"icon\">\n" +
+        "                            <i class=\"fa fa-headphones-simple\"></i>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "</footer>\n" +
+        "<div class=\"copy-right\">\n" +
+        "    <p>حقوق الطبع والنشر © لعام 2022 محفوظة لمتجر <span>متجر</span></p>\n" +
+        "</div>" ;
+}
